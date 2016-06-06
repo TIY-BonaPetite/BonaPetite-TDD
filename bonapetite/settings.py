5fb02a5ec0c3f285236d817f1410dedce1a40412
Greg Johnston <greg.a.johnston@gmail.com>
2016-06-06 02:30:41 -0400

    Updated permission settings

--- bonapetite/settings.py
+++ bonapetite/settings.py
@@ -30,7 +30,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES':
-        ('rest_framework.permissions.IsAutheticated', ),
+        ('rest_framework.permissions.IsAuthenticated', ),
     'PAGE_SIZE': 10
 }
 
